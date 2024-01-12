811de2bfb253c2ccd665743742c0b2f97ddb22a3
Varsha-Chaubey <varshachaubey63@gmail.com>
2024-01-12 09:35:21 +0530

    added PROD URL

--- db.ts
+++ db.ts
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        let URL = process.env.DBURL;
-        await mongoose.connect(`${URL}`);
+        let URL = process.env.PROD ==="PROD_TRUE"? process.env.PRODDBURL :process.env.DBURL;
+        const resp=await mongoose.connect(`${URL}`);
+       console.log(resp)
         console.log("Connected to DB");
     } catch (err) {
         console.error("Error connecting to DB:", err);
